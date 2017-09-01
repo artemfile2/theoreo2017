789de421b92f2147cd558c01ece92416b8390e2f
Tianatis <lin321@mail.ru>
2017-09-01 22:46:18 +0300

    fix bugs

--- routes/web/ajax.php
+++ routes/web/ajax.php
@@ -12,10 +12,10 @@ Route::group(['prefix' => 'ajax'],function () {
     /* Ajax для Админки */
     Route::group(['namespace' => 'Admin', 'prefix' => 'admin', 'middleware' => 'admin:admin_access'],function () {
 
-        Route::post('/admin/category/add', 'AjaxController@categoryAddPost')
+        Route::post('/category/add', 'AjaxController@categoryAddPost')
             ->name('ajax.admin.categoryAddPost');
 
-        Route::post('/admin/tag/add', 'AjaxController@tagAddPost')
+        Route::post('/tag/add', 'AjaxController@tagAddPost')
             ->name('ajax.admin.tagAddPost');
 
     });
